d1806b4f9db5ce0e3f8efba16ec2ead16b6ca082
Stefano Baccianella <4247706+mangiucugna@users.noreply.github.com>
2024-05-27 08:08:37 +0200

    [minor] add a guard to avoid an exception, although today this won't be triggered we won't know in the future if this gets refactored how it will change

--- src/json_repair/json_repair.py
+++ src/json_repair/json_repair.py
@@ -493,7 +493,7 @@ class JSONParser:
     def parse_boolean_or_null(self) -> Union[bool, str, None]:
         # <boolean> is one of the literal strings 'true', 'false', or 'null' (unquoted)
         starting_index = self.index
-        char = self.get_char_at().lower()
+        char = (self.get_char_at() or "").lower()
         if char == "t":
             value = ("true", True)
         elif char == "f":
@@ -506,7 +506,7 @@ class JSONParser:
             while char and i < len(value[0]) and char == value[0][i]:
                 i += 1
                 self.index += 1
-                char = self.get_char_at().lower()
+                char = (self.get_char_at() or "").lower()
             if i == len(value[0]):
                 return value[1]
 
